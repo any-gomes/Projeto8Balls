53722675d7833173ee37d3aee0cafe598143e26e
FernandesGuilherme <guilherme.logus@gmail.com>
2022-04-19 16:59:54 -0300

    Inserção de dados no banco

---
+++ src/main/java/MeuRemedio/app/controllers/CadastroUsuarioController.java
@@ -0,0 +1,4 @@
+package MeuRemedio.app.controllers;
+
+public class CadastroUsuarioController {
+}
--- src/main/java/MeuRemedio/app/controllers/LoginController.java
+++ src/main/java/MeuRemedio/app/controllers/LoginController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping(value = "/")
-    public String index(){
+    public String index() {
         return "index";
     }
+
 }
