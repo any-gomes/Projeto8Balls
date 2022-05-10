45bfa4e81d77d4a702e8f05eaca8872911c6dd9a
EricJSCabral <eric.jscabral@gmail.com>
2022-05-10 02:10:37 -0300

    Teste.

--- src/main/java/MeuRemedio/LanguageConfiguration.java
+++ src/main/java/MeuRemedio/LanguageConfiguration.java
@@ -3,10 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package MeuRemedio;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,17 +13,11 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 @Configuration
 public class LanguageConfiguration implements WebMvcConfigurer {
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
+
     @Bean
     public LocaleResolver localeResolver(){
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(new Locale("pt", "br"));
+        localeResolver.setDefaultLocale(Locale.US);
         return  localeResolver;
     }
     @Bean
