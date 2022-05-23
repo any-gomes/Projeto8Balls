281495692a704806ed8ae0f9170940b11a4f229d
PedroHaka <pedrohaka94@gmail.com>
2022-05-22 22:36:35 -0300

    Página html CadastroRemedio

--- src/main/java/MeuRemedio/MeuRemedioApplication.java
+++ src/main/java/MeuRemedio/MeuRemedioApplication.java
@@ -1,15 +1,15 @@
 package MeuRemedio;
 
 import MeuRemedio.app.controllers.IndexController;
-import  	 org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MeuRemedioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeuRemedioApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(MeuRemedioApplication.class, args);
 
-	}
+    }
 
 }
