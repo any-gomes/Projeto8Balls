20e661058544e6c9e98250916ac321caba82df52
EricJSCabral <eric.jscabral@gmail.com>
2022-05-29 01:29:53 -0300

    Ajustando configurações de inter.

--- src/main/java/MeuRemedio/app/configuration/LanguageConfiguration.java
+++ src/main/java/MeuRemedio/app/configuration/LanguageConfiguration.java
@@ -3,8 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package MeuRemedio.app.configuration;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,7 +19,9 @@ public class LanguageConfiguration implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver(){
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(new Locale("pt", "br"));
+        localeResolver.setDefaultLocale(new Locale("pt", "BR"));
+        localeResolver.setLocaleAttributeName("session.current.locale");
+        localeResolver.setTimeZoneAttributeName("session.current.timezone");
         return  localeResolver;
     }
     @Bean
@@ -25,6 +29,14 @@ public class LanguageConfiguration implements WebMvcConfigurer {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         return localeChangeInterceptor;
+    }
+
+    @Bean("messageSource")
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("languages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
