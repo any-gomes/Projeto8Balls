ea5842cdb1b7ac29250dc3dccaf74d5f5ce2b7c5
EricJSCabral <eric.jscabral@gmail.com>
2022-06-05 00:04:48 -0300

    Salvando agendamento e exibindo listagem.

--- src/main/java/MeuRemedio/app/repository/RemedioRepository.java
+++ src/main/java/MeuRemedio/app/repository/RemedioRepository.java
@@ -4,9 +4,11 @@ import MeuRemedio.app.models.remedios.Remedio;
 import MeuRemedio.app.models.usuarios.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RemedioRepository extends CrudRepository<Remedio, Long>  {
     Remedio findById (long id);
-    Iterable<Remedio> findAllByUsuario(Usuario usuario);
+    List<Remedio> findAllByUsuario(Usuario usuario);
     Iterable<Remedio> findAllByUsuario(Long id);
 
 }
